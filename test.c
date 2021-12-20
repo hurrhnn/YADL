f2619b6a2115e2873667e5b18fbbcd96356a9284
hurrhnn <20sunrin022@sunrint.hs.kr>
2021-12-20 14:04:27 +0900

    Added test YADL code.

---
+++ test.c
@@ -0,0 +1,30 @@
+/*
+ YADL is a Yet Another Discord Library, written from the scratch.
+ This file is part of YADL.
+
+ Copyright (C) 2021 Rhnn Hur (hurrhnn), Wonjun Jung (KokoseiJ)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "src/yadl.h"
+
+int main() {
+    yadl_context_t yadl_context;
+    memset(&yadl_context, 0x0, sizeof(yadl_context_t));
+    yadl_init(&yadl_context);
+
+    yadl_launch(&yadl_context);
+    return 0;
+}
