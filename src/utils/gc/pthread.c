328d9140673b5e04fcf87759b50e1038509fbb03
hurrhnn <20sunrin022@sunrint.hs.kr>
2021-12-20 14:00:36 +0900

    Added custom creation pthread code that used to mark and sweep.

---
+++ src/utils/gc/pthread.c
@@ -0,0 +1,55 @@
+/*
+ YADL is a Yet Another Discord Library, written from the scratch.
+ This file is part of YADL.
+
+ Copyright (C) 2021 Rhnn Hur (hurrhnn), Wonjun Jung (KokoseiJ)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "pthread.h"
+#include "gc.h"
+
+void *yadl_pthread_create(void *function, void *args) {
+    yadl_pthread_context_t *pthread_context = malloc(sizeof(yadl_pthread_context_t));
+    *pthread_context = (yadl_pthread_context_t) {malloc(sizeof(pthread_t)),
+                                                 malloc(sizeof(pthread_attr_t)),
+                                                 malloc(sizeof(pthread_mutex_t)),
+                                                 malloc(sizeof(pthread_cond_t))};
+
+    pthread_attr_init(pthread_context->pthread_attr);
+    pthread_mutex_init(pthread_context->pthread_mutex, NULL);
+    pthread_cond_init(pthread_context->pthread_cond, NULL);
+
+    pthread_attr_setstacksize(pthread_context->pthread_attr, YADL_LARGE_SIZE * 2);
+    pthread_create(pthread_context->pthread, pthread_context->pthread_attr, function, args);
+
+    if (yadl_gc_get_context_value(YADL_GC_NODE_PTHREAD) != NULL)
+        yadl_gc_append(pthread_context, YADL_GC_NODE_PTHREAD);
+    return pthread_context;
+}
+
+void yadl_pthread_append(pthread_t pthread) {
+    yadl_pthread_context_t *pthread_context = malloc(sizeof(yadl_pthread_context_t));
+    *pthread_context = (yadl_pthread_context_t) {malloc(sizeof(pthread_t)), NULL, NULL, NULL};
+    memcpy(pthread_context->pthread, &pthread, sizeof(pthread_t));
+    yadl_gc_append(pthread_context, YADL_GC_NODE_PTHREAD);
+}
+
+void yadl_thread_remove(yadl_pthread_context_t *pthread_context) {
+    free(pthread_context->pthread);
+    free(pthread_context->pthread_attr);
+    free(pthread_context->pthread_mutex);
+    free(pthread_context->pthread_cond);
+}
---
+++ src/utils/gc/pthread.h
@@ -0,0 +1,48 @@
+/*
+ YADL is a Yet Another Discord Library, written from the scratch.
+ This file is part of YADL.
+
+ Copyright (C) 2021 Rhnn Hur (hurrhnn), Wonjun Jung (KokoseiJ)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef YADL_PTHREAD_H
+#define YADL_PTHREAD_H
+
+#define _GNU_SOURCE
+#define __USE_GNU
+
+#include <signal.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "../utils.h"
+
+typedef struct yadl_pthread_context {
+    pthread_t *pthread;
+    pthread_attr_t *pthread_attr;
+    pthread_mutex_t *pthread_mutex;
+    pthread_cond_t *pthread_cond;
+} yadl_pthread_context_t;
+
+void *yadl_pthread_create(void *function, void *args);
+
+void yadl_pthread_append(pthread_t pthread);
+
+void yadl_thread_remove(yadl_pthread_context_t *pthread_context);
+
+#endif
