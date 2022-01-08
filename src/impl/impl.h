1907b22f3c68bc12b982000ec9f9ee6aa79d66e3
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-01-08 19:39:48 +0900

    Added `YADL_GATEWAY_INTENTS` enum.

--- src/impl/impl.h
+++ src/impl/impl.h
@@ -18,7 +18,34 @@
     along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
+#include "callback.h"
+#include "guild.h"
+#include "linked_list.h"
+#include "user.h"
+
 #ifndef YADL_IMPL_H
 #define YADL_IMPL_H
+
+#define YADL_GATEWAY_INTENTS_DEFAULT 32509
+
+enum YADL_GATEWAY_INTENTS {
+    GUILDS                    = (1 << 0),
+    GUILD_MEMBERS             = (1 << 1),
+    GUILD_BANS                = (1 << 2),
+    GUILD_EMOJIS_AND_STICKERS = (1 << 3),
+    GUILD_INTEGRATIONS        = (1 << 4),
+    GUILD_WEBHOOKS            = (1 << 5),
+    GUILD_INVITES             = (1 << 6),
+    GUILD_VOICE_STATES        = (1 << 7),
+    GUILD_PRESENCES           = (1 << 8),
+    GUILD_MESSAGES            = (1 << 9),
+    GUILD_MESSAGE_REACTIONS   = (1 << 10),
+    GUILD_MESSAGE_TYPING      = (1 << 11),
+    DIRECT_MESSAGES           = (1 << 12),
+    DIRECT_MESSAGE_REACTIONS  = (1 << 13),
+    DIRECT_MESSAGE_TYPING     = (1 << 14)
+} YADL_GATEWAY_INTENTS;
+
+#define YADL_GATEWAY_INTENTS_ALL 32767
 
 #endif //YADL_IMPL_H
