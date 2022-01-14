67db75468715382f9a63759132735655f7711f76
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-01-15 02:22:18 +0900

    Implemented some callback structs.

--- src/callback/callback.h
+++ src/callback/callback.h
@@ -25,6 +25,8 @@
 #include "../impl/impl.h"
 #include "../impl/user.h"
 #include "../impl/guild.h"
+#include "../impl/message.h"
+#include "../impl/channel.h"
 
 typedef struct yadl_context yadl_context_t;
 
@@ -32,24 +34,53 @@ typedef struct yadl_object_metadata yadl_object_metadata_t;
 
 struct yadl_event_on_ready {
     yadl_context_t *context;
+
     int8_t gateway_version;
     self_user_t *self_user;
     u_int16_t unavailable_guild_count;
 };
 
-typedef void (*yadl_event_on_ready_t)(const struct yadl_event_on_ready *event, void *user_data);
+typedef void (*yadl_event_on_ready_t)(const struct yadl_event_on_ready *event);
 
 struct yadl_event_on_guild_create {
     yadl_context_t *context;
+
     guild_t *guild;
 };
 
-typedef void (*yadl_event_on_guild_create_t)(const struct yadl_event_on_guild_create *event, void *user_data);
+typedef void (*yadl_event_on_guild_create_t)(const struct yadl_event_on_guild_create *event);
 
-typedef struct yadl_event_cb {
+struct yadl_event_on_direct_message_create {
+    yadl_context_t *context;
+
+    char* message_id;
+    user_t *author;
+    message_t *message;
+    channel_t *channel;
+};
+
+typedef void (*yadl_event_on_direct_message_create_t)(const struct yadl_event_on_direct_message_create *event);
+
+struct yadl_event_on_guild_message_create {
+    yadl_context_t *context;
+
+    char* message_id;
+    user_t *author;
+    guild_member_t *member;
+    message_t *message;
+    channel_t *channel;
+    guild_t *guild;
+    bool is_webhook_message;
+};
+
+typedef void (*yadl_event_on_guild_message_create_t)(const struct yadl_event_on_guild_message_create *event);
+
+typedef struct __attribute__((__packed__)) {
     yadl_event_on_ready_t on_ready;
+    yadl_event_on_direct_message_create_t on_direct_message_create;
+
     yadl_event_on_guild_create_t on_guild_create;
-    void *user_data;
+    yadl_event_on_guild_message_create_t on_guild_message_create;
 } yadl_event_cb_t;
 
 yadl_object_metadata_t *yadl_get_object_metadata(size_t type);
