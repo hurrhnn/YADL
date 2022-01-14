67b271ca974bd73d6f08012ff2ec5252b76db14d
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-01-15 02:13:50 +0900

    Implemented application objects serialize. (not fully yet.)

--- src/impl/application.c
+++ src/impl/application.c
@@ -19,10 +19,12 @@
 */
 
 #include "application.h"
+#include "impl.h"
 
 application_t *parse_application(JSON_Value *application_value) {
     JSON_Object *application = json_object(application_value);
-    application_t *result = yadl_malloc(sizeof(application_t), true);
+    application_t *result = yadl_malloc(sizeof(application_t));
+
     *result = (application_t) {(char *) json_object_get_string(application, "id"),
                                (char *) json_object_get_string(application, "name"),
                                (char *) json_object_get_string(application, "icon"),
@@ -32,10 +34,10 @@ application_t *parse_application(JSON_Value *application_value) {
                                yadl_json_boolean_null_check(json_object_get_boolean(application, "bot_require_code_grant")),
                                (char *) json_object_get_string(application, "terms_of_service_url"),
                                (char *) json_object_get_string(application, "privacy_policy_url"),
-                               (char *) json_serialize_to_string_pretty(json_object_get_wrapping_value(json_object_get_object(application, "owner"))),
+                               parse_user(json_object_get_wrapping_value(json_object_get_object(application, "owner"))),
                                (char *) json_object_get_string(application, "summary"),
                                (char *) json_object_get_string(application, "verify_key"),
-                               (char *) json_serialize_to_string_pretty(json_object_get_wrapping_value(json_object_get_object(application, "team"))),
+                               parse_team(json_object_get_wrapping_value(json_object_get_object(application, "team"))),
                                (char *) json_object_get_string(application, "guild_id"),
                                (char *) json_object_get_string(application, "primary_sku_id"),
                                (char *) json_object_get_string(application, "slug"),
@@ -44,4 +46,79 @@ application_t *parse_application(JSON_Value *application_value) {
     };
 
     return result;
+}
+
+team_t *parse_team(JSON_Value *team_value) {
+    JSON_Object *team = json_object(team_value);
+    team_t *result = yadl_malloc(sizeof(team_t));
+
+    *result = (team_t) {(char *) json_object_get_string(team, "icon"),
+                        (char *) json_object_get_string(team, "id"),
+                        yadl_object_array_parse(YADL_OBJECT_TEAM_MEMBER, json_object_get_array(team, "members")),
+                        (char *) json_object_get_string(team, "name"),
+                        (char *) json_object_get_string(team, "owner_user_id")
+    };
+
+    return result;
+}
+
+team_member_t *parse_team_member(JSON_Value *team_member_value) {
+    JSON_Object *team_member = json_object(team_member_value);
+    team_member_t *result = yadl_malloc(sizeof(team_member_t));
+
+    *result = (team_member_t) {(int) json_object_get_number(team_member, "membership_state"),
+                               (char *) json_object_get_string(team_member, "team_id"),
+                               parse_user(json_object_get_wrapping_value(json_object_get_object(team_member, "user")))
+    };
+
+    return result;
+}
+
+JSON_Value *struct_team_member(team_member_t *team_member) {
+    JSON_Object *result = yadl_json_object_builder(NULL);
+
+    json_object_set_number(result, "membership_state", team_member->membership_state);
+    json_object_set_string(result, "team_id", team_member->team_id);
+    json_object_set_value(result, "user", struct_user(team_member->user));
+
+    return json_object_get_wrapping_value(result);
+}
+JSON_Value *struct_team(team_t *team) {
+    JSON_Object *result = yadl_json_object_builder(NULL);
+
+    json_object_set_string(result, "icon", team->icon);
+    json_object_set_string(result, "id", team->id);
+
+    JSON_Array *members = yadl_json_array_builder(NULL);
+    for (int i=0; i<team->members->size; i++)
+        json_array_append_value(members, struct_team_member((team_member_t *)team->members->array[i]));
+
+    json_object_set_string(result, "name", team->name);
+    json_object_set_string(result, "owner_user_id", team->owner_user_id);
+
+    return json_object_get_wrapping_value(result);
+}
+JSON_Value *struct_application(application_t *application) {
+    JSON_Object *result = yadl_json_object_builder(NULL);
+
+    json_object_set_string(result, "id", application->id);
+    json_object_set_string(result, "name", application->name);
+    json_object_set_string(result, "icon", application->icon);
+    json_object_set_string(result, "description", application->description);
+    json_object_set_string(result, "rpc_origins", application->rpc_origins);
+    json_object_set_boolean(result, "bot_public", application->bot_public);
+    json_object_set_boolean(result, "bot_require_code_grant", application->bot_require_code_grant);
+    json_object_set_string(result, "terms_of_service_url", application->terms_of_service_url);
+    json_object_set_string(result, "privacy_policy_url", application->privacy_policy_url);
+    json_object_set_value(result, "owner", struct_user(application->owner));
+    json_object_set_string(result, "summary", application->summary);
+    json_object_set_string(result, "verify_key", application->verify_key);
+    json_object_set_value(result, "team", struct_team(application->team));
+    json_object_set_string(result, "guild_id", application->guild_id);
+    json_object_set_string(result, "primary_sku_id", application->primary_sku_id);
+    json_object_set_string(result, "slug", application->slug);
+    json_object_set_string(result, "cover_image", application->cover_image);
+    json_object_set_number(result, "flags", application->flags);
+
+    return json_object_get_wrapping_value(result);
 }
--- src/impl/application.h
+++ src/impl/application.h
@@ -23,8 +23,40 @@
 
 #include <stdbool.h>
 #include "parson.h"
+#include "user.h"
 #include "../utils/utils.h"
 #include "../json/json.h"
+
+typedef struct yadl_object_array yadl_object_array_t;
+
+typedef union {
+    struct {
+        u_int16_t UNUSED_BLANK                    : 12;
+        u_int8_t GATEWAY_PRESENCE                 : 1;
+        u_int8_t GATEWAY_PRESENCE_LIMITED         : 1;
+        u_int8_t GATEWAY_GUILD_MEMBERS            : 1;
+        u_int8_t GATEWAY_GUILD_MEMBERS_LIMITED    : 1;
+        u_int8_t VERIFICATION_PENDING_GUILD_LIMIT : 1;
+        u_int8_t EMBEDDED                         : 1;
+        u_int8_t GATEWAY_MESSAGE_CONTENT          : 1;
+        u_int8_t GATEWAY_MESSAGE_CONTENT_LIMITED  : 1;
+    } flags;
+    u_int32_t value;
+} yadl_application_flags_t;
+
+typedef struct __attribute__((__packed__)) {
+    int membership_state;
+    char* team_id;
+    user_t *user;
+} team_member_t;
+
+typedef struct __attribute__((__packed__)) {
+    char* icon;
+    char* id;
+    yadl_object_array_t *members;
+    char* name;
+    char* owner_user_id;
+} team_t;
 
 typedef struct __attribute__((__packed__)) {
     char *id;
@@ -33,16 +65,19 @@ typedef struct __attribute__((__packed__)) {
     char *description;
     char *rpc_origins;
     /* array of strings */
+
     bool bot_public;
     bool bot_require_code_grant;
     char *terms_of_service_url;
     char *privacy_policy_url;
-    char *owner;
+    user_t *owner;
     /* partial user object */
+
     char *summary;
     char *verify_key;
-    char *team;
+    team_t *team;
     /* team object */
+
     char *guild_id;
     char *primary_sku_id;
     char *slug;
@@ -50,5 +85,17 @@ typedef struct __attribute__((__packed__)) {
     int flags;
 
 } application_t;
+
+application_t *parse_application(JSON_Value *application_value);
+
+team_t *parse_team(JSON_Value *team_value);
+
+team_member_t *parse_team_member(JSON_Value *team_member_value);
+
+JSON_Value *struct_team_member(team_member_t *team_member);
+
+JSON_Value *struct_team(team_t *team);
+
+JSON_Value *struct_application(application_t *application);
 
 #endif //YADL_APPLICATION_H
