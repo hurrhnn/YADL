2455f070ee0aaeef10e15b2ca2dd3786cf4956e1
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-01-21 08:57:26 +0900

    Added LICENSE text.

--- src/raw/voice_udp_client.h
+++ src/raw/voice_udp_client.h
@@ -1,3 +1,23 @@
+/*
+ YADL is Yet Another Discord Library, written from the scratch.
+ This file is part of YADL.
+
+ Copyright (C) 2021 Rhnn Hur (hurrhnn), Wonjun Jung (KokoseiJ)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef YADL_VOICE_UDP_CLIENT_H
 #define YADL_VOICE_UDP_CLIENT_H
 
